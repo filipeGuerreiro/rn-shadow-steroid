642ac5a1004e8bb1dab7ba6eddd6c9e1ae9f2458
filipeGuerreiro <filipe.m.guerreiro@gmail.com>
2020-11-21 19:48:22 +0900

    Add definitions to core ns to match RN API

--- src/steroid/rn/core.cljs
+++ src/steroid/rn/core.cljs
@@ -3,6 +3,11 @@
   (:require ["react-native" :as rn]
             [reagent.core :as reagent]
             [steroid.rn.components.basic :as basic]
+            [steroid.rn.components.other :as other]
+            [steroid.rn.components.list :as list]
+            [steroid.rn.components.status-bar :as status-bar]
+            [steroid.rn.components.touchable :as touchable]
+            [steroid.rn.components.ui :as ui]
             [steroid.rn.reloader :as reloader]))
 
 (def debug? ^boolean js/goog.DEBUG)
@@ -14,6 +19,24 @@
 (def image basic/image)
 (def text-input basic/text-input)
 (def scroll-view basic/scroll-view)
+
+(def activity-indicator other/activity-indicator)
+(def keyboard-avoiding-view other/keyboard-avoiding-view)
+(def modal other/modal)
+(def refresh-control other/refresh-control)
+
+(def flat-list list/flat-list)
+(def section-list list/section-list)
+
+(def status-bar status-bar/status-bar)
+
+(def touchable-highlight touchable/touchable-highlight)
+(def touchable-native-feedback touchable/touchable-native-feedback)
+(def touchable-opacity touchable/touchable-opacity)
+(def touchable-without-feedback touchable/touchable-without-feedback)
+
+(def button ui/button)
+(def switch ui/switch)
 
 (def reload-view reloader/reload-view)
 
